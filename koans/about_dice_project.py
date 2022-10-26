a340715dd8445c90c0fa2425087d3592284da699
Alec Hansen <alecdhansen@gmail.com>
2022-10-25 23:24:50 -0400

    Lessons #22-27

--- koans/about_dice_project.py
+++ koans/about_dice_project.py
@@ -5,18 +5,22 @@ from runner.koan import *
 
 import random
 
+
 class DiceSet:
     def __init__(self):
         self._values = None
+
+    # https://docs.python.org/3/library/functions.html?highlight=property#property
 
     @property
     def values(self):
         return self._values
 
     def roll(self, n):
-        # Needs implementing!
-        # Tip: random.randint(min, max) can be used to generate random numbers
-        pass
+        self._values = []
+        for i in range(n):
+            self._values.append(random.randint(1, 6))
+
 
 class AboutDiceProject(Koan):
     def test_can_create_a_dice_set(self):
@@ -27,10 +31,14 @@ class AboutDiceProject(Koan):
         dice = DiceSet()
 
         dice.roll(5)
+        print("values are: ", dice.values)
         self.assertTrue(isinstance(dice.values, list), "should be a list")
         self.assertEqual(5, len(dice.values))
         for value in dice.values:
-            self.assertTrue(value >= 1 and value <= 6, "value " + str(value) + " must be between 1 and 6")
+            self.assertTrue(
+                value >= 1 and value <= 6,
+                "value " + str(value) + " must be between 1 and 6",
+            )
 
     def test_dice_values_do_not_change_unless_explicitly_rolled(self):
         dice = DiceSet()
@@ -48,8 +56,7 @@ class AboutDiceProject(Koan):
         dice.roll(5)
         second_time = dice.values
 
-        self.assertNotEqual(first_time, second_time, \
-            "Two rolls should not be equal")
+        self.assertNotEqual(first_time, second_time, "Two rolls should not be equal")
 
         # THINK ABOUT IT:
         #
